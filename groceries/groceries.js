5f1bea264de98febfcd8c25674127189c9305b9c
Andrew Baik <and.baik@gmail.com>
2016-03-14 12:16:10 -0400

    2

--- groceries/groceries.js
+++ groceries/groceries.js
@@ -4,10 +4,10 @@ gList = [];
 
 $('form').on("click", ".submit", function() {
   console.log($(".item-text").attributes.val());
-  glist.push($(".item-text").attributes.val());
+  gList.push($(".item-text").attributes.val());
 });
 
 
-for (var i = 0; i < glist.length; i++) {
+for (var i = 0; i < gList.length; i++) {
   $(".listy ul").append("<li>" + glist[i] + "</li>");
 }
